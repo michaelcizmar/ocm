0cc6f2b06175732ecbc2ce8a7767a1066df6c3f4
Aaron Worley <aaron@pikasoftware.com>
2014-10-01 15:00:33 -0400

    Removed debug code.

--- cms-custom/extensions/case_tabs/case_tabs.php
+++ cms-custom/extensions/case_tabs/case_tabs.php
@@ -7,36 +7,30 @@ function case_tabs_extension()
 	if ("Mediator" == $_SESSION['def_group'])
 	{
 		$m = pl_menu_get('case_tabs_med');
-		echo "1";
 	}
 	
 	else if ("Med Mgr" == $_SESSION['def_group'])
 	{
 		$m = pl_menu_get('case_tabs_medmgr');
-		echo "2";
 	}
 	
 	else if ("95" == $_SESSION['def_office'])
 	{
-		echo "3";
 		$m = pl_menu_get('case_tabs_hrh');
 	}
 	
 	else if (("system" == $_SESSION['def_group']) &&  ("80" == $_SESSION['def_office']))
 	{
-		echo "4";
 		$m = pl_menu_get('case_tabs_medmgr');
 	}
 	
 	else if (("default" == $_SESSION['def_group']) &&  ("80" == $_SESSION['def_office']))
 	{
-		echo "5";
 		$m = pl_menu_get('case_tabs_slh');
 	}
 	
 	else
 	{
-		echo "6";
 		$m = pl_menu_get('case_tabs');
 	}
 	
