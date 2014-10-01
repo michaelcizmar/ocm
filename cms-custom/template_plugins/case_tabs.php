5b216310cbfe3b37323e3255eeb62c870b31189d
Aaron Worley <aaron@pikasoftware.com>
2014-10-01 12:11:22 -0400

    Case tab modifications for LSNC.

--- cms-custom/template_plugins/case_tabs.php
+++ cms-custom/template_plugins/case_tabs.php
@@ -2,6 +2,58 @@
 
 function case_tabs($field_name = null, $field_value = null, $menu_array = null, $args = null)
 {
+	// Custom code for LSNC; it decides which case tab table to use.
+	if ("Mediator" == $_SESSION['def_group'])
+	{
+		$m = pl_menu_get('case_tabs_med');
+		echo "1";
+	}
+	
+	else if ("Med Mgr" == $_SESSION['def_group'])
+	{
+		$m = pl_menu_get('case_tabs_medmgr');
+		echo "2";
+	}
+	
+	else if ("95" == $_SESSION['def_office'])
+	{
+		echo "3";
+		$m = pl_menu_get('case_tabs_hrh');
+	}
+	
+	else if (("system" == $_SESSION['def_group']) &&  ("80" == $_SESSION['def_office']))
+	{
+		echo "4";
+		$m = pl_menu_get('case_tabs_medmgr');
+	}
+	
+	else if (("default" == $_SESSION['def_group']) &&  ("80" == $_SESSION['def_office']))
+	{
+		echo "5";
+		$m = pl_menu_get('case_tabs_slh');
+	}
+	
+	else
+	{
+		echo "6";
+		$m = pl_menu_get('case_tabs');
+	}
+	echo $_SESSION['def_office'];
+	
+	$i = 0;
+	foreach($m as $key => $val)
+	{
+		$menu_array['case-' .$key . '.php'] = array('file' => 'case-' .$key . '.php',
+													'name' => $val,
+													'enabled' => "1",
+													'tab_id' => "$i",
+													'tab_order' => "$i",
+													'tab_row' => "1");
+		$i++;
+	}
+	
+	// End custom code for LSNC.
+
 	
 	if (!is_array($menu_array)){
 		$menu_array = array();
@@ -35,49 +87,7 @@ function case_tabs($field_name = null, $field_value = null, $menu_array = null, 
 	if(isset($menu_array["case-{$field_name}.php"]['autosave']) && $menu_array["case-{$field_name}.php"]['autosave'])
 	{
 		$autosave = true;
-	}
-	
-	
-	// Custom code for LSNC; it decides which case tab table to use.
-	$i = 0;
-	if ("Mediator" == $_SESSION['def_group'])
-	{
-		$menu_array = pl_menu_get('case_tabs_med');
-		echo $i++;
-	}
-	
-	else if ("Med Mgr" == $_SESSION['def_group'])
-	{
-		$menu_array = pl_menu_get('case_tabs_medmgr');
-		echo $i++;
-	}
-	
-	else if ("95" == $_SESSION['def_office'])
-	{
-		echo $i++;
-		$menu_array = pl_menu_get('case_tabs_hrh');
-	}
-	
-	else if (("system" == $_SESSION['def_group']) &&  ("80" == $_SESSION['def_office']))
-	{
-		echo $i++;
-		$menu_array = pl_menu_get('case_tabs_medmgr');
-	}
-	
-	else if (("default" == $_SESSION['def_group']) &&  ("80" == $_SESSION['def_office']))
-	{
-		echo $i++;
-		$menu_array = pl_menu_get('case_tabs_slh');
-	}
-	
-	else
-	{
-		echo $i++;
-		$menu_array = pl_menu_get('case_tabs');
-	}
-echo "hi world;";
-	// End custom code for LSNC.
-	
+	}	
 	
 	
 	foreach ($menu_array as $key => $tab)
