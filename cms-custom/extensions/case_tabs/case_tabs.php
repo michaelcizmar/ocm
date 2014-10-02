63502c8b4725eb66033e0e049e6effc977e0fc9d
Aaron Worley <aaron@pikasoftware.com>
2014-10-02 13:51:47 -0400

    Updated office code-to-menu mapping based on phone call with Mark.

--- cms-custom/extensions/case_tabs/case_tabs.php
+++ cms-custom/extensions/case_tabs/case_tabs.php
@@ -4,27 +4,12 @@
 
 function case_tabs_extension()
 {	
-	if ("Mediator" == $_SESSION['def_group'])
-	{
-		$m = pl_menu_get('case_tabs_med');
-	}
-	
-	else if ("Med Mgr" == $_SESSION['def_group'])
-	{
-		$m = pl_menu_get('case_tabs_medmgr');
-	}
-	
-	else if ("95" == $_SESSION['def_office'])
+	if ("95" == $_SESSION['def_office'])
 	{
 		$m = pl_menu_get('case_tabs_hrh');
 	}
-	
-	else if (("system" == $_SESSION['def_group']) &&  ("80" == $_SESSION['def_office']))
-	{
-		$m = pl_menu_get('case_tabs_medmgr');
-	}
-	
-	else if (("default" == $_SESSION['def_group']) &&  ("80" == $_SESSION['def_office']))
+
+	else if ("80" == $_SESSION['def_office'])
 	{
 		$m = pl_menu_get('case_tabs_slh');
 	}
