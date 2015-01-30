931fa8eca7269d91c42f36696e5d07ceff21903b
Aaron Worley <aaron@pikasoftware.com>
2015-01-30 15:02:00 -0500

    Changed version to 6.03.

--- cms/pika-danio.php
+++ cms/pika-danio.php
@@ -12,7 +12,7 @@ $auth_row = array();
 // CONSTANTS
 if(!defined('PIKA_VERSION'))   {  define('PIKA_VERSION', '6');       }
 if(!defined('PIKA_REVISION'))  {  define('PIKA_REVISION', '0');     }
-if(!defined('PIKA_PATCH_LEVEL'))  {  define('PIKA_PATCH_LEVEL', '2');     }
+if(!defined('PIKA_PATCH_LEVEL'))  {  define('PIKA_PATCH_LEVEL', '3');     }
 if(!defined('PIKA_CODE_NAME')) {  define('PIKA_CODE_NAME', 'danio'); }
 
 
