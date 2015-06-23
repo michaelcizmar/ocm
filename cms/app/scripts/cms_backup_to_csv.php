2f7f47d91afa77307937778c5342d8361f8ddc2d
Aaron Worley <aaron@pikasoftware.com>
2015-06-23 13:39:58 -0400

    Improved the readability of the CSV download messages.

--- cms/app/scripts/cms_backup_to_csv.php
+++ cms/app/scripts/cms_backup_to_csv.php
@@ -7,7 +7,7 @@ $save_folder_path = '/tmp';
 
 $c = curl_init();
 curl_setopt($c, CURLOPT_URL, $url . '/services/table_listing.php');
-echo $url . '/services/table_listing.php';
+echo "Connecting to {$url}\n";
 curl_setopt($c, CURLOPT_TIMEOUT, 60);
 curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($c, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
@@ -17,13 +17,11 @@ curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 2);
 $status_code = curl_getinfo($c, CURLINFO_HTTP_CODE);
 $result=curl_exec($c);
 curl_close ($c);
-echo $status_code;
-echo "\n";
 $result = json_decode($result);
 
 foreach ($result as $v)
 {
-	echo $v ."\n";
+	echo "Table {$v} ";
 	$c = curl_init();
 	curl_setopt($c, CURLOPT_URL, $url . '/services/csv.php?action=' . $v);
 	curl_setopt($c, CURLOPT_TIMEOUT, 60);
@@ -36,12 +34,8 @@ foreach ($result as $v)
 	$result=curl_exec($c);
 	curl_close ($c);
 	$file_path = $save_folder_path . '/' . $v . '.csv';
-	echo $file_path;
 	file_put_contents($file_path, $result);
-	echo "\n";
+	echo "saved to {$file_path}\n";
 }
 
-
-
-	
 ?>
