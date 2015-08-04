c550630fb193ae1789da099698bb949c14eac72e
Aaron Worley <aaron@pikasoftware.com>
2015-08-03 21:09:08 -0400

    Upon review, it looks like we don’t need last_addr and last_active in the database after all.

--- cms/app/sql/upgrades/pika604.sql
+++ cms/app/sql/upgrades/pika604.sql
@@ -1,5 +1,3 @@
 ALTER TABLE cases MODIFY veteran_household tinyint(4) default NULL;
 ALTER TABLE counters MODIFY id char(32) NOT NULL DEFAULT 'COUNTERNAME';
 ALTER TABLE menu_sp_problem MODIFY menu_order smallint NOT NULL DEFAULT '0';
-ALTER TABLE users ADD last_addr varchar(50) DEFAULT NULL AFTER session_data,
-ADD last_active varchar(11) DEFAULT NULL AFTER last_addr;
