5e88b68b075dc335af5544e9eaa950316d496c4f
Aaron Worley <aaron@pikasoftware.com>
2015-08-03 20:59:59 -0400

    Included changes that hadn’t been added to the upgrade file yet.

--- cms/app/sql/upgrades/pika604.sql
+++ cms/app/sql/upgrades/pika604.sql
@@ -1,1 +1,6 @@
 ALTER TABLE cases MODIFY veteran_household tinyint(4) default NULL;
+ALTER TABLE activities ADD last_changed_user_id int(11) DEFAULT NULL AFTER created;
+ALTER TABLE counters MODIFY id char(32) NOT NULL DEFAULT 'COUNTERNAME';
+ALTER TABLE menu_sp_problem MODIFY menu_order smallint NOT NULL DEFAULT '0';
+ALTER TABLE users ADD last_addr varchar(50) DEFAULT NULL AFTER session_data,
+ADD last_active varchar(11) DEFAULT NULL AFTER last_addr;
