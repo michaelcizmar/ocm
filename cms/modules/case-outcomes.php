d3eaf86e3654fd58d26da3ee9e247d434725e32d
Aaron Worley <aaron@pikasoftware.com>
2015-09-25 13:13:46 -0400

    Fixes and additions to get the converted wireframe form to record and retrieve data correctly.

--- cms/modules/case-outcomes.php
+++ cms/modules/case-outcomes.php
@@ -20,10 +20,19 @@ else
 	//echo $sql;
 	$sql = "select a.outcome_goal_id, a.goal, b.result 
 			from outcome_goals AS a
-			LEFT JOIN outcomes AS b USING (goal)
-			where problem in ('0', '6') order by outcome_goal_order ASC";
+			LEFT JOIN outcomes AS b USING (outcome_goal_id)
+			where problem in ('0', '6') AND case_id = {$case_row['case_id']} order by outcome_goal_order ASC"; //echo $sql; exit();
+	$result = mysql_query($sql) or trigger_error(mysql_error($result));
+
+	if (mysql_num_rows($result) == 0)
+	{
+		$sql = "select a.outcome_goal_id, a.goal, 2 AS result 
+				from outcome_goals AS a
+				where problem in ('0', '6') order by outcome_goal_order ASC"; //echo $sql; exit();
+		$result = mysql_query($sql) or trigger_error(mysql_error($result));		
+	}
 	
-	$result = mysql_query($sql);
+	
 	$C .= "<form action=\"{$base_url}/ops/update_case.php\" method=\"POST\">";
 	$C .= "<table class=\"table table-striped\">\n";
 	$i = 0;
@@ -38,12 +47,13 @@ else
 			$na_checked = "";
 			break;
 			
-			case 1:
+			case 0:
 			$yes_checked = "";
 			$no_checked = " checked";
 			$na_checked = "";
 			break;
-
+			
+			case 2:
 			default:
 			$yes_checked = "";
 			$no_checked = "";
@@ -53,25 +63,25 @@ else
 		
 		
 		$C .= "<tr>\n";
-		$C .= "<td>{$row['goal']}</td>\n";
 		$C .= "<td>
 		<label class=\"radio inline\">
 		<input type=\"radio\" name=\"outcomes[{$row['outcome_goal_id']}]\" id=\"optionsRadios{$i}\" value=\"1\"{$yes_checked}>
 		Yes</label>
 		<label class=\"radio inline\">
-		<input type=\"radio\" name=\"outcomes[{$row['outcome_goal_id']}]\" id=\"optionsRadios{$i}\" value=\"0\"($no_checked}>
+		<input type=\"radio\" name=\"outcomes[{$row['outcome_goal_id']}]\" id=\"optionsRadios{$i}\" value=\"0\"{$no_checked}>
 		No</label>
 		<label class=\"radio inline\">
 		<input type=\"radio\" name=\"outcomes[{$row['outcome_goal_id']}]\" id=\"optionsRadios{$i}\" value=\"2\"{$na_checked}>
 		N/A</label>
 		</td>\n";
+		$C .= "<td>{$row['goal']}</td>\n";
 		$C .= "</tr>\n";
 		$i++;
 	}
 	
 		$C .= "<tr>\n";
+		$C .= "<td><textarea rows=\"5\" class=\"span3\" name=\"outcome_notes\" tabindex=\"1\">{$case_row['outcome_notes']}</textarea></td>\n";
 		$C .= "<td>What other significant outcome?</td>\n";
-		$C .= "<td><textarea rows=\"5\" class=\"span2\" name=\"outcome_notes\" tabindex=\"1\">{$case_row['outcome_notes']}</textarea></td>\n";
 		$C .= "</tr>\n";
 
 	$C .= "</table>\n";
