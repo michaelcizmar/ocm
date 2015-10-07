bb8c18af1c46a5f29153831bc20081000205ff28
Aaron Worley <aaron@pikasoftware.com>
2015-10-07 12:29:41 -0400

    Changed database to use UNIX timestamps instead of MySQL timestamps for authentication session tracking.  This should prevent time zone errors.

--- cms/app/sql/upgrades/pika605.sql
+++ cms/app/sql/upgrades/pika605.sql
@@ -1,1 +1,9 @@
 INSERT INTO `settings` VALUES ('autofill_time_funding','1');
+START TRANSACTION;
+ALTER TABLE user_sessions CHANGE created old_created TIMESTAMP,
+	CHANGE last_updated old_last_updated TIMESTAMP;
+ALTER TABLE user_sessions ADD created INT, ADD last_updated INT;
+UPDATE user_sessions SET created = UNIX_TIMESTAMP(old_created),
+	last_updated = UNIX_TIMESTAMP(old_last_updated);
+ALTER TABLE user_sessions DROP old_created, DROP old_last_updated;
+COMMIT;
