92e8a61ce188ec70e944d5a31cb3426e56fe3d42
Aaron Worley <aaron@pikasoftware.com>
2015-10-15 14:29:58 -0400

    Added outcome module schema changes to upgrade file.

--- cms/app/sql/upgrades/pika605.sql
+++ cms/app/sql/upgrades/pika605.sql
@@ -7,3 +7,39 @@ UPDATE user_sessions SET created = UNIX_TIMESTAMP(old_created),
 	last_updated = UNIX_TIMESTAMP(old_last_updated);
 ALTER TABLE user_sessions DROP old_created, DROP old_last_updated;
 COMMIT;
+
+--
+-- Table structure for table outcome_goals
+--
+
+CREATE TABLE outcome_goals (
+  outcome_goal_id INT NOT NULL default '0',
+  goal CHAR(128),
+  problem CHAR(2),
+  active TINYINT,
+  outcome_goal_order INT,
+  PRIMARY KEY  (outcome_definition_id),
+  KEY problem (problem)
+) ENGINE = INNODB;
+
+--
+-- Table structure for table outcome_goals
+--
+
+CREATE TABLE outcomes (
+  outcome_id INT NOT NULL default '0',
+  case_id INT,
+  outcome_goal_id INT,
+  result TINYINT,
+  PRIMARY KEY  (outcome_id),
+  KEY case_id (case_id)
+) ENGINE = INNODB;
+
+ALTER TABLE cases
+ADD COLUMN outcome_notes TEXT default NULL AFTER outcome,
+ADD COLUMN outcome_income_before MEDIUMINT default NULL AFTER outcome,
+ADD COLUMN outcome_income_after MEDIUMINT default NULL AFTER outcome_income_before,
+ADD COLUMN outcome_assets_before MEDIUMINT default NULL AFTER outcome_income_after,
+ADD COLUMN outcome_assets_after MEDIUMINT default NULL AFTER outcome_assets_before,
+ADD COLUMN outcome_debt_before MEDIUMINT default NULL AFTER outcome_assets_after,
+ADD COLUMN outcome_debt_after MEDIUMINT default NULL AFTER outcome_debt_before;
