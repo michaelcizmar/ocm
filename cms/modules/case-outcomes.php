a6c7048afaecda36c64ba06ccf24bcf043fce7b8
Aaron Worley <aaron@pikasoftware.com>
2015-10-15 13:45:01 -0400

    Replaced placeholder problem category/code in SQL with the values specified via GET.

--- cms/modules/case-outcomes.php
+++ cms/modules/case-outcomes.php
@@ -13,7 +13,7 @@ else
 	// AMW - I think there are some single digit problem codes floating around.
 	// This is a workaround for them.
 	$problem_code = str_pad($case_row['problem'], 2, "0", STR_PAD_LEFT);
-	$problem_category = substr($problem_code, 0, 1);
+	$problem_category = substr($problem_code, 0, 1) . 'X';
 	$sql = "SELECT goal, result, outcome_definition_id FROM
 		outcome_goals LEFT JOIN outcomes USING(outcome_definition_id)
 		WHERE (case_id={$case_row['case_id']} OR case_id IS NULL) 
@@ -23,15 +23,15 @@ else
 	$sql = "select a.outcome_goal_id, a.goal, b.result 
 			from outcome_goals AS a
 			LEFT JOIN outcomes AS b USING (outcome_goal_id)
-			where problem in ('0', '6') AND case_id = {$case_row['case_id']} 
-			order by problem DESC, outcome_goal_order ASC"; // echo $sql; exit();
+			where problem in ('{$problem_category}', '{$problem_code}') AND case_id = {$case_row['case_id']} 
+			order by problem DESC, outcome_goal_order ASC";  echo $sql; //exit();
 	$result = mysql_query($sql) or trigger_error(mysql_error($result));
 
 	if (mysql_num_rows($result) == 0)
 	{
 		$sql = "select a.outcome_goal_id, a.goal, 2 AS result 
 				from outcome_goals AS a
-				where problem in ('0', '6') order by outcome_goal_order ASC"; //echo $sql; exit();
+				where problem in ('{$problem_category}', '{$problem_code}') order by outcome_goal_order ASC"; echo $sql; //exit();
 		$result = mysql_query($sql) or trigger_error(mysql_error($result));		
 	}
 	
