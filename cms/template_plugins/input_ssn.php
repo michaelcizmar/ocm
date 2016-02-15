89575ba5efae256c5657d4b8ab0a3bebe65fa4f6
Aaron Worley <aaron@pikasoftware.com>
2016-02-15 14:22:41 -0500

    Added SSN field template plugin that is aware of truncated or removed SSNs.

---
+++ cms/template_plugins/input_ssn.php
@@ -0,0 +1,42 @@
+<?php
+function input_ssn($field_name = null, $field_value = null, $menu_array = null, $args = null) 
+{
+	$C = '';
+	$ssn_type = null;
+	$result = mysql_query("DESCRIBE contacts") or trigger_error(mysql_error());
+	
+	while ($row = mysql_fetch_assoc($result))
+	{
+		if ($row['Field'] == 'ssn')
+		{
+			$ssn_type = $row['Type'];
+		}
+	}
+	
+	if ($ssn_type == 'enum(\'deactivated\')')
+	{
+		return '';
+	}
+	
+	$C .= "SSN:<br/>\n";
+	
+	if ($ssn_type == 'varchar(4)' || $ssn_type == 'char(4)')
+	{
+		$C .= '<input type="text" name="ssn" value="' . htmlentities($field_value) . '" maxlength="4" size="22" tabindex="1">';
+		
+	}
+	
+	else
+	{
+		$C .= '<input type="text" name="ssn" onkeyup="pika_ssn(this);" value="' . htmlentities($field_value) . '" maxlength="11" size="22" tabindex="1">';
+	}
+
+	$C .= "<br/>\n";
+	
+	return $C;
+}
+
+
+
+
+?>
