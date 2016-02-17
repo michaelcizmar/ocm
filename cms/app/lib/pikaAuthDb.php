607a568f23f10fee4396cb3d63c6ad2d16044315
Aaron Worley <aaron@pikasoftware.com>
2016-02-16 22:04:05 -0500

    Pull password value from database, and compare to provided password in PHP land now.  Determine whether the saved value was made with password_hash or md5, and convert all md5 to password_hash.

--- cms/app/lib/pikaAuthDb.php
+++ cms/app/lib/pikaAuthDb.php
@@ -71,32 +71,40 @@ class pikaAuthDb
 		if(!is_null($identity) && strlen($identity) > 0 && strlen($this->table_name) > 0)
 		{
 			$safe_identity = mysql_real_escape_string($identity);
-			if(!is_null($this->credential_funct) && function_exists($this->credential_funct))
-			{
-				$cf = $this->credential_funct;
-				$credential = $cf($credential);
-			}
-			$safe_credential = mysql_real_escape_string($credential);
-			$sql  = "SELECT user_id, username, enabled, password_expire, users.group_id AS group_name, groups.*
+			
+			$sql  = "SELECT user_id, username, enabled, password_expire, 
+					users.group_id AS group_name, groups.*, password
 					FROM {$this->table_name}
 					LEFT JOIN groups ON users.group_id=groups.group_id
-					WHERE 1
-					AND enabled = '1'
-					AND username='{$safe_identity}'";
-			$sql_safe = $sql . " AND password='XXXXXXXX' LIMIT 1;";
-			$sql .= " AND password='{$safe_credential}' LIMIT 1;";
-			$result = mysql_query($sql) or trigger_error("SQL: " . $sql_safe . " Error: " . mysql_error());
-			// one user record matched the username and password
+					WHERE enabled = '1'
+					AND username='{$safe_identity}'
+					AND LENGTH(password) > 0";
+			$result = mysql_query($sql) or trigger_error("SQL: " . $sql . " Error: " . mysql_error());
+			
 			if (mysql_num_rows($result) == 1)
-			{  // Identity & Credential match existing records - allow login
-				$this->is_authorized = true;
-				$this->auth_row = mysql_fetch_assoc($result);
+			{
+				// one user record matched the username and password
+				if (password_verify($credential, $row['password']))
+				{  // Identity & Credential match existing records - allow login
+					$this->is_authorized = true;
+					$this->auth_row = mysql_fetch_assoc($result);
+				}
+				
+				else if (md5($credential) == $row['password'])
+				{
+					require_once('pikaUser.php');
+					$u = new pikaUser($user_id);
+					$u->setValue('password', password_hash($credential, PASSWORD_DEFAULT));
+					$u->save();
+				}
+				
+				else 
+				{  // No matching user credentials found - pass login error			
+					$msgstr = 'The Login Credentials you supplied are invalid.  Please re-check your Username and Password and try again.';
+					$this->setMessage('0100',$msgstr,__FILE__,__LINE__);
+				}
 			}
-			else 
-			{  // No matching user credentials found - pass login error			
-				$msgstr = 'The Login Credentials you supplied are invalid.  Please re-check your Username and Password and try again.';
-				$this->setMessage('0100',$msgstr,__FILE__,__LINE__);
-			}
+			
 		}
 		elseif(!is_null($identity))
 		{ 
