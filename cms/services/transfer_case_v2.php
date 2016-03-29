3cbc190c3e14bcfcdc669d3894c283b58bb9c90a
Aaron Worley <aaron@pikasoftware.com>
2016-03-29 13:53:37 -0400

    Fixed typos that were silently dropping case transfer values.

--- cms/services/transfer_case_v2.php
+++ cms/services/transfer_case_v2.php
@@ -16,9 +16,9 @@ require_once('pikaTransfer.php');
 
 $auth_row = pikaAuthHttp::getInstance()->getAuthRow();
 $transfer = new pikaTransfer;
-$tranfer->user_id = $auth_row['user_id'];
+$transfer->user_id = $auth_row['user_id'];
 $transfer->json_data = file_get_contents('php://input');
-$tranfer->accepted = 2;  // Pending review.
+$transfer->accepted = 2;  // Pending review.
 $transfer->save();
 echo '1';
 exit();
