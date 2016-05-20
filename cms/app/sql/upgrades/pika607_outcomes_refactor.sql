2b5e995238b53cb390c0ea90e16e1c83ed4fcbdb
Aaron Worley <aaron@pikasoftware.com>
2016-05-20 14:17:30 -0400

    Add SQL columns needed for CA IOLTA outcomes.

--- cms/app/sql/upgrades/pika607_outcomes_refactor.sql
+++ cms/app/sql/upgrades/pika607_outcomes_refactor.sql
@@ -5,3 +5,11 @@ CHANGE outcome_assets_before outcome_assets_after_service MEDIUMINT default NULL
 CHANGE outcome_assets_after outcome_assets_no_service MEDIUMINT default NULL,
 CHANGE outcome_debt_before outcome_debt_after_service MEDIUMINT default NULL,
 CHANGE outcome_debt_after outcome_debt_no_service MEDIUMINT default NULL;
+
+ALTER TABLE cases 
+ADD COLUMN ca_outcome_amount_obtained MEDIUMINT default NULL AFTER outcome_debt_no_service,
+ADD COLUMN ca_outcome_monthly_obtained MEDIUMINT default NULL AFTER ca_outcome_amount_obtained,
+ADD COLUMN ca_outcome_amount_reduced MEDIUMINT default NULL AFTER ca_outcome_monthly_obtained,
+ADD COLUMN ca_outcome_monthly_reduced MEDIUMINT default NULL AFTER ca_outcome_amount_reduced;
+
+INSERT INTO `settings` VALUES ('ca_iolta_outcomes', '0');
