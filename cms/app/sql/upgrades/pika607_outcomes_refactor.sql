b23bc5c57d01873ef298e7bb7357c7018d7fbe3a
Aaron Worley <aaron@pikasoftware.com>
2016-05-24 14:47:34 -0400

    Added online schema change version of the SQL upgrade.
    Not sure this will be its official location, may split it off into its own file before release.

--- cms/app/sql/upgrades/pika607_outcomes_refactor.sql
+++ cms/app/sql/upgrades/pika607_outcomes_refactor.sql
@@ -13,3 +13,11 @@ ADD COLUMN ca_outcome_amount_reduced MEDIUMINT default NULL AFTER ca_outcome_mon
 ADD COLUMN ca_outcome_monthly_reduced MEDIUMINT default NULL AFTER ca_outcome_amount_reduced;
 
 INSERT INTO `settings` VALUES ('ca_iolta_outcomes', '0');
+
+#pt-online-schema-change --dry-run --alter "CHANGE outcome_income_before outcome_income_after_service MEDIUMINT default NULL, \
+#CHANGE outcome_income_after outcome_income_no_service MEDIUMINT default NULL, \
+#CHANGE outcome_assets_before outcome_assets_after_service MEDIUMINT default NULL, \
+#CHANGE outcome_assets_after outcome_assets_no_service MEDIUMINT default NULL, \
+#CHANGE outcome_debt_before outcome_debt_after_service MEDIUMINT default NULL, \
+#CHANGE outcome_debt_after outcome_debt_no_service MEDIUMINT default NULL" D=wtls,t=cases
+ 
