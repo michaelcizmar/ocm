5d34f6ea1f1014627b5f34df0b4621453bc56a67
Aaron Worley <aaron@pikasoftware.com>
2016-09-15 11:21:25 -0400

    Removed experimental table from upgrade script.

--- cms/app/sql/upgrades/pika607_name_search.sql
+++ cms/app/sql/upgrades/pika607_name_search.sql
@@ -1,7 +1,2 @@
 ALTER TABLE aliases ADD COLUMN keywords TEXT;
 ALTER TABLE aliases ADD FULLTEXT(keywords);
-CREATE TABLE name_variants (
-  first_name char(255) NOT NULL default '',
-  root_name char(255) NOT NULL default '',
-  PRIMARY KEY  (first_name)
-) ENGINE=InnoDB;
